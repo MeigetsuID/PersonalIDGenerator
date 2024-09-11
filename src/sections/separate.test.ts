40e81f5a863a530fd0dfe282801321442ab30eae
Kamioda <kamiodamasato@outlook.com>
2024-09-11 23:46:00 +0900

    【section 4】2つのSHA512文字列を結合した文字列を4等分するコードを追加( #1 )

--- src/sections/separate.test.ts
+++ src/sections/separate.test.ts
@@ -1,4 +1,4 @@
-import { separate } from './separate';
+import { separate, separateTwoMergedSHA512Text } from './separate';
 
 describe('separate', () => {
     it('length is odd', () => {
@@ -6,5 +6,20 @@ describe('separate', () => {
     });
     it('length is even', () => {
         expect(separate('abcd')).toEqual(['ab', 'cd']);
+    });
+});
+
+describe('separateTwoMergedSHA512Text', () => {
+    it('valid/lower', () => {
+        expect(separateTwoMergedSHA512Text('a'.repeat(256))).toEqual(['a'.repeat(64), 'a'.repeat(64), 'a'.repeat(64), 'a'.repeat(64)]);
+    });
+    it('valid/upper', () => {
+        expect(separateTwoMergedSHA512Text('A'.repeat(256))).toEqual(['A'.repeat(64), 'A'.repeat(64), 'A'.repeat(64), 'A'.repeat(64)]);
+    });
+    it('invalid/length error', () => {
+        expect(() => separateTwoMergedSHA512Text('a'.repeat(255))).toThrow('invalid two merged SHA512 text');
+    });
+    it('invalid/character error', () => {
+        expect(() => separateTwoMergedSHA512Text('g'.repeat(256))).toThrow('invalid two merged SHA512 text');
     });
 });
--- src/sections/separate.ts
+++ src/sections/separate.ts
@@ -2,3 +2,13 @@ export function separate(str: string): string[] {
     const pLen = str.length / 2 + Number(str.length % 2 === 1);
     return [str.slice(0, pLen), str.slice(pLen)];
 }
+
+const TwoMergedSHA512TextReg = /^([0-9a-f]|[0-9A-F]){256}$/;
+
+export function separateTwoMergedSHA512Text(str: string): string[] {
+    if (!TwoMergedSHA512TextReg.test(str)) throw new Error('invalid two merged SHA512 text');
+    /* v8 ignore next 2 */
+    const strs = str.match(/.{1,64}/g) || [];
+    if (strs.length !== 4) throw new Error('Split Error');
+    return strs;
+}
