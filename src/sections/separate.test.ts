ec32cafe823d3e5366799faafce890b8bfe517ff
Kamioda <kamiodamasato@outlook.com>
2024-09-11 23:56:44 +0900

    【section 6】2つのSHA512文字列を結合した文字列を8等分するコードを追加( #1 )
    用途自体はSHA512の文字列の入れ替えをやった後の文字列を8等分するものだが、記載用途にも使えなくはないので特に問題は無い

--- src/sections/separate.test.ts
+++ src/sections/separate.test.ts
@@ -1,4 +1,4 @@
-import { separate, separateTwoMergedSHA512Text_4 } from './separate';
+import { separate, separateTwoMergedSHA512Text_4, separateTwoMergedSHA512Text_8 } from './separate';
 
 describe('separate', () => {
     it('length is odd', () => {
@@ -21,5 +21,38 @@ describe('separateTwoMergedSHA512Text_4', () => {
     });
     it('invalid/character error', () => {
         expect(() => separateTwoMergedSHA512Text_4('g'.repeat(256))).toThrow('invalid two merged SHA512 text');
+    });
+});
+
+describe('separateTwoMergedSHA512Text_8', () => {
+    it('valid/lower', () => {
+        expect(separateTwoMergedSHA512Text_8('a'.repeat(256))).toStrictEqual([
+            'a'.repeat(32),
+            'a'.repeat(32), 
+            'a'.repeat(32),
+            'a'.repeat(32),
+            'a'.repeat(32),
+            'a'.repeat(32),
+            'a'.repeat(32),
+            'a'.repeat(32),
+        ]);
+    });
+    it('valid/upper', () => {
+        expect(separateTwoMergedSHA512Text_8('A'.repeat(256))).toStrictEqual([
+            'A'.repeat(32),
+            'A'.repeat(32),
+            'A'.repeat(32),
+            'A'.repeat(32),
+            'A'.repeat(32),
+            'A'.repeat(32),
+            'A'.repeat(32),
+            'A'.repeat(32),
+        ]);
+    });
+    it('invalid/length error', () => {
+        expect(() => separateTwoMergedSHA512Text_8('a'.repeat(255))).toThrow('invalid two merged SHA512 text');
+    });
+    it('invalid/character error', () => {
+        expect(() => separateTwoMergedSHA512Text_8('g'.repeat(256))).toThrow('invalid two merged SHA512 text');
     });
 });
--- src/sections/separate.ts
+++ src/sections/separate.ts
@@ -12,3 +12,11 @@ export function separateTwoMergedSHA512Text_4(str: string): string[] {
     if (strs.length !== 4) throw new Error('Split Error');
     return strs;
 }
+
+export function separateTwoMergedSHA512Text_8(str: string): string[] {
+    if (!TwoMergedSHA512TextReg.test(str)) throw new Error('invalid two merged SHA512 text');
+    /* v8 ignore next 2 */
+    const strs = str.match(/.{1,32}/g) || [];
+    if (strs.length !== 8) throw new Error('Split Error');
+    return strs;
+}
