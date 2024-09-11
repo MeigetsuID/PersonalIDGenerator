06185dbc404f8e7a1c842fbc4344965ed74b25b4
Kamioda <kamiodamasato@outlook.com>
2024-09-11 23:24:10 +0900

    【section 5, 7】偶数個の文字列の配列の順番を入れ替えて結合するコードの追加( #1 )

--- src/sections/swapandmerge.test.ts
+++ src/sections/swapandmerge.test.ts
@@ -1,10 +1,22 @@
-import { SwapAndMerge } from "./swapandmerge";
+import { SwapAndMerge, SwapAndMerge_Multiple } from './swapandmerge';
 
-describe("SwapAndMerge", () => {
-    it("length is 2", () => {
-        expect(SwapAndMerge(["abc", "def"])).toBe("defabc");
+describe('SwapAndMerge', () => {
+    it('length is 2', () => {
+        expect(SwapAndMerge(['abc', 'def'])).toBe('defabc');
     });
-    it("length is not 2", () => {
-        expect(() => SwapAndMerge(["abc"])).toThrow("string array length is not 2");
+    it('length is not 2', () => {
+        expect(() => SwapAndMerge(['abc'])).toThrow('string array length is not 2');
+    });
+});
+
+describe('SwapAndMerge_Multiple', () => {
+    it('length is even', () => {
+        expect(SwapAndMerge_Multiple(['abc', 'def', 'ghi', 'jkl'])).toStrictEqual(['defabc', 'jklghi']);
+    });
+    it('length is not even', () => {
+        expect(() => SwapAndMerge_Multiple(['abc', 'def', 'ghi'])).toThrow('string array length is not even');
+    });
+    it('length is 0', () => {
+        expect(() => SwapAndMerge_Multiple([])).toThrow('string array is empty');
     });
 });
--- src/sections/swapandmerge.ts
+++ src/sections/swapandmerge.ts
@@ -2,3 +2,13 @@ export function SwapAndMerge(strs: string[]): string {
     if (strs.length !== 2) throw new Error('string array length is not 2');
     return strs[1] + strs[0];
 }
+
+export function SwapAndMerge_Multiple(strs: string[]): string[] {
+    if (strs.length === 0) throw new Error('string array is empty');
+    if (strs.length % 2 !== 0) throw new Error('string array length is not even');
+    const result: string[] = [];
+    for (let i = 0; i < strs.length; i += 2) {
+        result.push(SwapAndMerge([strs[i], strs[i + 1]]));
+    }
+    return result;
+}
