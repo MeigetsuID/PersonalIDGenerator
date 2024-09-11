5629013c942e525c0558b0505a7129ad5ae8d6ab
Kamioda <kamiodamasato@outlook.com>
2024-09-11 23:51:34 +0900

    toEqual->toStrictEqual( #1 )

--- src/sections/separate.test.ts
+++ src/sections/separate.test.ts
@@ -2,19 +2,19 @@ import { separate, separateTwoMergedSHA512Text_4 } from './separate';
 
 describe('separate', () => {
     it('length is odd', () => {
-        expect(separate('abcde')).toEqual(['abc', 'de']);
+        expect(separate('abcde')).toStrictEqual(['abc', 'de']);
     });
     it('length is even', () => {
-        expect(separate('abcd')).toEqual(['ab', 'cd']);
+        expect(separate('abcd')).toStrictEqual(['ab', 'cd']);
     });
 });
 
 describe('separateTwoMergedSHA512Text_4', () => {
     it('valid/lower', () => {
-        expect(separateTwoMergedSHA512Text_4('a'.repeat(256))).toEqual(['a'.repeat(64), 'a'.repeat(64), 'a'.repeat(64), 'a'.repeat(64)]);
+        expect(separateTwoMergedSHA512Text_4('a'.repeat(256))).toStrictEqual(['a'.repeat(64), 'a'.repeat(64), 'a'.repeat(64), 'a'.repeat(64)]);
     });
     it('valid/upper', () => {
-        expect(separateTwoMergedSHA512Text_4('A'.repeat(256))).toEqual(['A'.repeat(64), 'A'.repeat(64), 'A'.repeat(64), 'A'.repeat(64)]);
+        expect(separateTwoMergedSHA512Text_4('A'.repeat(256))).toStrictEqual(['A'.repeat(64), 'A'.repeat(64), 'A'.repeat(64), 'A'.repeat(64)]);
     });
     it('invalid/length error', () => {
         expect(() => separateTwoMergedSHA512Text_4('a'.repeat(255))).toThrow('invalid two merged SHA512 text');
