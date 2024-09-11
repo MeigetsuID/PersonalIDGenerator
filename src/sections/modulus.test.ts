69ca8c07546ae63158ef0e7ebeb588c8216c76bd
Kamioda <kamiodamasato@outlook.com>
2024-09-12 00:27:23 +0900

    【section 11】n文字目の数値に(n % 4) + 1をかけて総和を出すコードを追加( #1 )
    モジュラス10ウェイト3の演算方法を準用した演算方法を用いているので、仮名としてモジュラス10ウェイト4と名付けた

---
+++ src/sections/modulus.test.ts
@@ -0,0 +1,10 @@
+import { m10w4Total } from "./modulus";
+
+describe('m10w4Total', () => {
+    it('valid', () => {
+        expect(m10w4Total('1234567890')).toBe(109);
+    });
+    it('invalid/character error', () => {
+        expect(() => m10w4Total('123456789a')).toThrow('Invalid input');
+    });
+});
---
+++ src/sections/modulus.ts
@@ -0,0 +1,6 @@
+const AllowedPattern = /^[0-9]+$/;
+
+export function m10w4Total(str: string): number {
+    if (!AllowedPattern.test(str)) throw new Error('Invalid input');
+    return [...str].map(i => parseInt(i)).reduce((acc, cur, idx) => acc + cur * (idx % 4 + 1));
+}
