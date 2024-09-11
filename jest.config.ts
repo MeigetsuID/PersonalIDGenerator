55305f54f8477319eda7b3687aea2f64cc060c04
Kamioda <kamiodamasato@outlook.com>
2024-09-11 22:57:19 +0900

    jestのコンフィグを追加( #1 )

---
+++ jest.config.ts
@@ -0,0 +1,21 @@
+/**
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+import type { Config } from 'jest';
+
+const config: Config = {
+    clearMocks: true,
+    collectCoverage: true,
+    coverageDirectory: 'coverage',
+    coverageProvider: 'v8',
+    transform: {
+        '^.+\\.[tj]sx?$': 'ts-jest',
+    },
+    transformIgnorePatterns: ['node_modules/(?!(nodeeasyfileio))'],
+    testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
+    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+};
+
+export default config;
