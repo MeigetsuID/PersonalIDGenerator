f8f9dec1d387be75ad7b4930706842f252f1dada
Kamioda <kamiodamasato@outlook.com>
2024-09-14 01:33:28 +0900

    マジックナンバーを生成するコードの追加( #1 )

---
+++ src/magicNumber/index.test.ts
@@ -0,0 +1,45 @@
+import { calcMagicNumber } from ".";
+
+const m10w3CheckDigit = (input: string): number => {
+    const m10w3Total = [...input]
+        .map((x) => parseInt(x))
+        .reverse()
+        .reduce((acc, cur, idx) => acc + cur * (3 - Number(idx % 2 === 1) * 2), 0);
+    return m10w3Total % 10 === 0
+        ? 0
+        : (10 - (m10w3Total % 10));
+};
+
+describe('Test Function Check', () => {
+    it('check digit is 0', () => {
+        expect(m10w3CheckDigit('20151119')).toBe(0);
+    });
+    it ('check digit is not 0', () => {
+        expect(m10w3CheckDigit('20151129')).toBe(9);
+    });
+});
+
+describe('calcMagicNumber', () => {
+    it('valid/magic number is 0', () => {
+        const IDBase = '78426633591';
+        expect(m10w3CheckDigit(`${IDBase}${calcMagicNumber(IDBase)}`)).toBe(0);
+    });
+
+    it('valid/magic number is not 0/pattern 1', () => {
+        const IDBase = '78426033591';
+        expect(m10w3CheckDigit(`${IDBase}${calcMagicNumber(IDBase)}`)).toBe(0);
+    });
+
+    it('valid/magic number is not 0/pattern 2', () => {
+        const IDBase = '78426033593';
+        expect(m10w3CheckDigit(`${IDBase}${calcMagicNumber(IDBase)}`)).toBe(0);
+    });
+
+    it('invalid/character error', () => {
+        expect(() => calcMagicNumber('2015111a9')).toThrow('Input must be 11 digits');
+    });
+
+    it('invalid/length error', () => {
+        expect(() => calcMagicNumber('20151119')).toThrow('Input must be 11 digits');
+    });
+});
---
+++ src/magicNumber/index.ts
@@ -0,0 +1,14 @@
+const AllowedPattern = /^[0-9]{11}$/;
+
+export function calcMagicNumber(IDBase: string): number {
+    if (!AllowedPattern.test(IDBase)) throw new Error('Input must be 11 digits');
+    const m10w3Total = [...IDBase]
+        .map((x) => parseInt(x))
+        .reverse()
+        .reduce((acc, cur, idx) => acc + cur * (1 + (idx % 2) * 2), 0);
+    if (m10w3Total % 10 === 0) return 0;
+    const Next10DivivableVal = (Math.floor(m10w3Total / 10) + 1) * 10;
+    const Diff = Next10DivivableVal - m10w3Total;
+    const DivMod = Diff % 3;
+    return (Diff + (DivMod === 0 ? 0 : (3 - DivMod) * 10)) / 3;
+}
